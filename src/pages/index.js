cde91c3c99a5130f271683df3bae927c4f738e42
Per Louie Clara <perlouieclara2000@gmail.com>
2022-06-10 18:36:28 +0800

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const style = {
     cursor: "pointer",
     fontSize: "20px",
     color: "black",
-    marginTop:"50px"
+    marginTop: "50px",
   },
 };
 
@@ -28,50 +28,61 @@ export default function index() {
     router.push("./");
   };
 
-  const gotoContact = () =>{
-    router.push("/contact")
-  }
+  const gotoContact = () => {
+    router.push("/contact");
+  };
 
-  const gotostudSignup = () =>{
-      router.push("./studentsignup")
-  }
+  const gotostudSignup = () => {
+    router.push("./studentsignup");
+  };
 
   return (
     <Box>
       <Head>
         <title>SDA | Homepage</title>
       </Head>
-      <Paper elevation={3} sx={{ backgroundColor: "#11CBD7", display:"flex"}}>
-        <Box sx={{marginLeft:"150px", marginTop:"10px",}}>
+      <Paper elevation={3} sx={{ backgroundColor: "#11CBD7", display: "flex" }}>
+        <Box sx={{ marginLeft: "150px", marginTop: "10px" }}>
           <img
-            sx={{ cursor: "pointer", }}
+            sx={{ cursor: "pointer" }}
             src="images/logo_school.png"
             height={100}
             width={100}
             onClick={gotoIndex}
           ></img>
-          </Box>
-          <Box sx={{display:"flex", marginLeft:"700px"}}>
-            <Typography sx={{ ...style.navi, fontWeight: "700" }}>
-              Home
-            </Typography>
-            <Typography sx={style.navi} onClick={studSignin}>
-              Login
-            </Typography>
-            <Typography sx={style.navi} onClick={gotostudSignup}>SignUp</Typography>
-            <Typography sx={style.navi} onClick={gotoContact}>Contact</Typography>
-            <Typography sx={style.navi}>About</Typography>
-            <Typography sx={style.navi} onClick={gotoTeacher}>
-              Teachers
-            </Typography>
-            </Box>
+        </Box>
+        <Box sx={{ display: "flex", marginLeft: "700px" }}>
+          <Typography sx={{ ...style.navi, fontWeight: "700" }}>
+            Home
+          </Typography>
+          <Typography sx={style.navi} onClick={studSignin}>
+            Login
+          </Typography>
+          <Typography sx={style.navi} onClick={gotostudSignup}>
+            SignUp
+          </Typography>
+          <Typography sx={style.navi} onClick={gotoContact}>
+            Contact
+          </Typography>
+          <Typography sx={style.navi}>About</Typography>
+          <Typography sx={style.navi} onClick={gotoTeacher}>
+            Teachers
+          </Typography>
+        </Box>
       </Paper>
-      <Box sx={{ display: "flex", textAlign: "center", marginTop: "20px", backgroundColor:"#F0FFF3" }}>
-        <Box sx={{ flexBasis: "50%", padding:"20px",}}>
+      <Box
+        sx={{
+          display: "flex",
+          textAlign: "center",
+          marginTop: "20px",
+          backgroundColor: "#F0FFF3",
+        }}
+      >
+        <Box sx={{ flexBasis: "50%", padding: "20px" }}>
           <Typography sx={{ fontSize: "40px", color: "#384259" }}>
             Vision Statement
           </Typography>
-          <Typography sx={{fontSize:"20px"}}>
+          <Typography sx={{ fontSize: "20px" }}>
             Our vision is to develop well rounded, confident and responsible
             individuals who aspire to achieve their full potential. We will do
             this by providing a welcoming, happy, safe, and supportive learning
@@ -79,26 +90,40 @@ export default function index() {
             celebrated.
           </Typography>
         </Box>
-        <Box sx={{ flexBasis: "50%", textAlign: "center", padding:"20px" }}>
+        <Box sx={{ flexBasis: "50%", textAlign: "center", padding: "20px" }}>
           <Typography sx={{ fontSize: "40px", color: "#384259" }}>
             Mission Statement
           </Typography>
-          <Typography sx={{fontSize:"20px"}}>
+          <Typography sx={{ fontSize: "20px" }}>
             We foster our students’ love for learning, encourage them to try new
             and exciting things, and give them a solid foundation to build on.
           </Typography>
         </Box>
       </Box>
-      <Box sx={{display:"flex", backgroundColor:"#F0FFF3"}}>
-      <Box sx={{padding:"20px", flexBasis:"50%"}}>
-        <img src="/images/xo.jpg" height={500} width={700}></img>
+      <Box sx={{ display: "flex", backgroundColor: "#F0FFF3" }}>
+        <Box sx={{ padding: "20px", flexBasis: "50%" }}>
+          <img src="/images/xo.jpg" height={500} width={700}></img>
+        </Box>
+        <Box sx={{ flexBasis: "50%" }}>
+          <img src="./images/R.png" width={700} height={100}></img>
+          <Typography sx={{ marginTop: "10px", fontSize: "60px" }}>
+            Be Known
+          </Typography>
+          <Typography sx={{ fontSize: "60px" }}>Be Anything</Typography>
+          <Typography sx={{ fontSize: "60px" }}>
+            Be The Best You Can Be
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{ marginTop: "10px", color: "white", backgroundColor: "red" }}
+            onClick={gotoContact}
+          >
+            Inquire Now
+          </Button>
+        </Box>
       </Box>
-      <Box sx={{flexBasis:"50%"}}>
-        <Typography sx={{marginTop:"80px", fontSize:"60px"}}>Be Known</Typography>
-        <Typography sx={{fontSize:"60px"}}>Be Anything</Typography>
-        <Typography sx={{fontSize:"60px"}}>Be The Best You Can Be</Typography>
-        <Button variant="contained" sx={{marginTop:"10px", color:"white", backgroundColor:"red"}} onClick={gotoContact}>Inquire Now</Button>
-      </Box>
+      <Box>
+        <Typography>What We do</Typography>
       </Box>
     </Box>
   );
